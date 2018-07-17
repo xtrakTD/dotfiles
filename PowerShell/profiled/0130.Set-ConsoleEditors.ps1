b8eb898d684223d148efa4e85dc5533ddedcc399
XTR <xtr@destiny.games>
2018-07-17 16:09:50 +0300

    Allow win paths in unix commands

--- PowerShell/profiled/0130.Set-ConsoleEditors.ps1
+++ PowerShell/profiled/0130.Set-ConsoleEditors.ps1
@@ -1,17 +1,34 @@
+function Get-UnixFilename {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)][string]$WinFileName
+    )
+
+    [string]$unixPath = $WinFileName.Replace('\', '/').Replace(':','')
+
+    if(!($unixPath.ToCharArray()[0] -eq '.')) {
+        $unixPath = '/' + $unixPath
+    }
+
+    return $unixPath
+}
 function nano {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true, ValueFromPipeline=$false)][string]$File
     )
 
-    bash -c "nano $File"
-}
+    $unixFile = Get-UnixFilename $File
 
+    bash -c "nano $unixFile"
+}
 function vim {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true, ValueFromPipeline=$false)][string]$File
     )
-    
-    bash -c "vim $File"
+
+    $unixFile = Get-UnixFilename $File
+
+    bash -c "vim $unixFile"
 }
